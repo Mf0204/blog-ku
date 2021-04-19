54dcf57371079b84976639a198e4771049ebd3a9
farid <faridmuchamad2001@gmail.com>
2021-04-19 13:40:47 +0700

    berfhasilkjhbkjh

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-// import styles from '../styles/Home.module.css'@
 import Jumbotron from '../components/jumbotron'
 import Article from '../components/article'
 import Pagination from '../components/pagination'
